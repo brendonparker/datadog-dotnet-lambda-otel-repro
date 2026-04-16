d761c3768bf77556ec2f46176e05531f953c99a0
Brendon Parker <brendon.parker@rfsmart.com>
2026-04-15 22:19:12 -0500

    Cleanup

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -1,6 +1,8 @@
 using System.Diagnostics;
 using DemoApi;
 
+const string TenantId = "tenant.id";
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.ConfigureOpenTelemetry();
@@ -9,20 +11,23 @@ builder.Services.AddAWSLambdaHosting(LambdaEventSource.HttpApi);
 
 var app = builder.Build();
 
-app.MapGet("/", async (ILogger<Program> log) =>
+app.MapGet("/", async () =>
 {
-    log.LogInformation("Activity.Current is null: {isNull}", Activity.Current == null);
-    Activity.Current?.SetTag("tenant.id", "acme_inc");
+    var isCurrentActivityIsNull = Activity.Current is null;
+    var tenantId = "acme_inc";
+    // This does not show - but I think it should.
+    Activity.Current?.SetTag(TenantId, tenantId);
     using (var _ = Tracing.Source.StartActivity("CustomOTELSpan"))
     {
-        Activity.Current?.SetTag("tenant.id", "acme_inc");
+        // This does show - since this is a custom span
+        Activity.Current?.SetTag(TenantId, tenantId);
         await Task.Delay(200);
     }
 
     return Results.Ok(new
     {
-        currentActivityIsNull = Activity.Current == null,
-        tenantId = "acme_inc"
+        isCurrentActivityIsNull,
+        tenantId
     });
 });
 
